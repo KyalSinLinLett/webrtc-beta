fd89df4cbb6136d576156d4c2a171ed8946445af
KyalSinLinLett <alexsin0012@gmail.com>
2021-11-15 21:10:57 +0630

    added STUN server

--- main.js
+++ main.js
@@ -21,7 +21,11 @@ const firestore = firebase.firestore();
 const servers = {
   iceServers: [
     {
-      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+      urls: [
+        "stun:stun1.l.google.com:19302",
+        "stun:stun3.l.google.com:19302",
+        "stun:stun4.l.google.com:19302"
+      ],
     },
   ],
   iceCandidatePoolSize: 10,
